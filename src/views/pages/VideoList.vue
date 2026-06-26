c3902cfa6268843d33d32cee62c09bd8ff23e903
c237878 <jiyang@vip.qq.com>
2026-06-27 00:16:33 +0800

    feat: 首页只显示file_size大于0的影片

--- src/views/pages/VideoList.vue
+++ src/views/pages/VideoList.vue
@@ -149,7 +149,8 @@ const loadVideos = async () => {
   try {
     const params = {
       pageIndex: page.value,
-      pageSize: pageSize.value
+      pageSize: pageSize.value,
+      hasFile: true  // 首页只显示有文件的影片
     }
     if (filters.value.category) params.category = filters.value.category
     if (filters.value.country) params.country = filters.value.country
